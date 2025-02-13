a64c8797ed5fd608a54120b6c070a2765c793f2c
Chandish Sipai <kce079bct021@khwopa.edu.np>
2025-02-13 09:17:36 +0545

    chat basic

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -3,6 +3,16 @@ import { Server } from "socket.io"
 export const socketHandler = (httpsServer) => {
     const io = new Server(httpsServer)
     io.on("connection", (socket) => {
-        console.log("socket connected")
+        console.log("socket connected");
+        let room;
+        socket.on("joinRoom", (roomObject) => {
+            console.log(roomObject);
+            socket.join(roomObject.roomId);
+            room = roomObject.roomId;
+
+        })
+        socket.on("sendMessage", (data) => {
+            io.to(room).emit("sendMessageServer", data);
+        })
     })
 }
